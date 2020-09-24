76a3e714f6fac60b00f03133265286d180adaadd
Ferenc Erki <erkiferenc@gmail.com>
2020-09-24 09:25:06 +0200

    Initial commit

---
+++ lib/Rex/Hook/File/Impostor.pm
@@ -0,0 +1,43 @@
+package Rex::Hook::File::Impostor;
+
+# ABSTRACT:
+
+use 5.010;
+use strict;
+use warnings;
+
+our $VERSION = '9999';
+
+1;
+
+__END__
+
+=for :stopwords CPAN
+
+=head1 SYNOPSIS
+
+    use Rex::Hook::File::Impostor;
+
+=head1 DESCRIPTION
+
+=head1 DIAGNOSTICS
+
+This module does not do any error checking (yet).
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+This module does not require any configuration, nor does it use any environment variables.
+
+=head1 DEPENDENCIES
+
+See the included C<cpanfile>.
+
+=head1 INCOMPATIBILITIES
+
+There are no known incompatibilities with other modules.
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs. Make sure they are reported.
+
+=cut
---
+++ t/module.t
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '9999';
+
+use Test2::V0;
